3d33f86d890722faf409c44b8f11530abb3cdb04
Cyril Ledru <cyril@lev-art.com>
2023-09-16 10:20:12 +0900

    Control homebrew with nix-darwin

--- configuration.nix
+++ configuration.nix
@@ -25,4 +25,38 @@
     name = "cyrilledru";
     home = "/Users/cyrilledru";
   };
+
+  # Requires Homebrew to be installed
+  system.activationScripts.preUserActivation.text = ''
+    if ! xcode-select --version 2>/dev/null; then
+      $DRY_RUN_CMD xcode-select --install
+    fi
+    if ! /usr/local/bin/brew --version 2>/dev/null; then
+      $DRY_RUN_CMD /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+  '';
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "none"; # Do not uninstall non-listed packages
+      upgrade = true;
+    };
+
+    brews = [
+    ];
+
+    casks = [
+      "anki"
+      "balenaetcher"
+      "firefox"
+      "p4v"
+      "postman"
+      "spotify"
+      "steam"
+      "transmission"
+      "vlc"
+    ];
+  };
 }
