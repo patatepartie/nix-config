8ca5736adf2da6a5e1536e7ca219abf75eedd537
Cyril Ledru <cyril@lev-art.com>
2023-09-16 10:48:49 +0900

    Rename deprecated variable

--- home.nix
+++ home.nix
@@ -173,7 +173,7 @@
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
 
     history.share = false;
 
