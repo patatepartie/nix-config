149dca0e3d6e5145eed4e6aa8e0da718a78c3a91
Cyril Ledru <cyril@lev-art.com>
2024-11-06 08:14:05 +0900

    Force the tmux shell to zsh
    For some reason, tmux starts sessions with sh as the shell, now.
    Using the programs.tmux.shell does not work, but the extra config does.

--- hosts/2023-macbook-pro/home.nix
+++ hosts/2023-macbook-pro/home.nix
@@ -181,6 +181,7 @@ in
     mouse = true;
 
     extraConfig = ''
+      set-option -g default-command "reattach-to-user-namespace -l zsh"
       bind-key -T copy-mode-vi 'y' send -X copy-pipe-and-cancel 'reattach-to-user-namespace pbcopy'
       bind-key -T copy-mode-vi Enter send -X copy-pipe-and-cancel 'reattach-to-user-namespace pbcopy'
     '';
