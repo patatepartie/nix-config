f0546000888ed107429c50defa3360d0b72913c3
Cyril Ledru <cyrilledru@Cyrils-2018-MacBook-Pro.local>
2025-03-20 19:51:25 +0900

    MBP2018 Better brew config

--- hosts/2018-macbook-pro/modules/apps.nix
+++ hosts/2018-macbook-pro/modules/apps.nix
@@ -1,11 +1,8 @@
-{ ... }: {
+{ config, ... }: {
   # Install homebrew and prerequisites if not already present
   system.activationScripts.preUserActivation.text = ''
     if ! xcode-select --version 2>/dev/null; then
       $DRY_RUN_CMD xcode-select --install
-    fi
-    if ! /usr/local/bin/brew --version 2>/dev/null; then
-      $DRY_RUN_CMD /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
   '';
 
@@ -13,9 +10,11 @@
     enable = true;
     onActivation = {
       autoUpdate = true;
-      cleanup = "none"; # Do not uninstall non-listed packages
+      cleanup = "zap";
       upgrade = true;
     };
+
+    taps = builtins.attrNames config.nix-homebrew.taps;
 
     brews = [
     ];
