eef0f65ec1fee10b91692511b4a1665306483968
Cyril Ledru <cyril@lev-art.com>
2025-03-23 17:50:27 +0900

    MBP2018 Split apps module into several files
    One to configure homebrew itself, one to configure vscode because it has several parts and one for the rest of the casks.
    If an app needs more than just its package name defined, then it should be moved to its own file.

---
+++ hosts/2018-macbook-pro/modules/apps/casks.nix
@@ -0,0 +1,13 @@
+{ ... }: {
+  homebrew.casks = [
+    "google-chrome"
+    "google-drive"
+    "firefox"
+    "microsoft-remote-desktop"
+    "obsidian"
+    "p4v"
+    "slack"
+    "spotify"
+    "vlc"
+  ];
+}
