90116652e88d6aa9d7fb0d773c272b056258162e
Cyril Ledru <cyril@lev-art.com>
2025-04-13 13:52:56 +0900

    MBP2018 - Fix style

--- hosts/2018-macbook-pro/modules/apps/vscode.nix
+++ hosts/2018-macbook-pro/modules/apps/vscode.nix
@@ -3,7 +3,8 @@
 
   system.activationScripts.postUserActivation.text = ''
     install_command="/usr/local/bin/code"
-    for package in esbenp.prettier-vscode github.copilot github.copilot-chat hashicorp.terraform jnoortheen.nix-ide mechatroner.rainbow-csv ms-python.python ms-vscode-remote.remote-containers redhat.vscode-yaml shopify.ruby-lsp; do
+    for package in esbenp.prettier-vscode github.copilot github.copilot-chat hashicorp.terraform jnoortheen.nix-ide \
+      mechatroner.rainbow-csv ms-python.python ms-vscode-remote.remote-containers redhat.vscode-yaml shopify.ruby-lsp; do
       install_command="$install_command  --install-extension $package"
     done
     eval "$install_command"
