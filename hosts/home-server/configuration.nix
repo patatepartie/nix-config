70a095f165ede2e13fe99db37f22d96e94b5de85
Cyril Ledru <cyril.ledru@gmail.com>
2025-11-03 21:51:58 +0900

    HomeServer - Enable SSH from net-ssh
    The etm MACs don't seem supported so we have to add some less safe ones.

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -129,7 +129,17 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-256"
+      ];
+    };
+  };
 
   services.mosquitto = {
     enable = true;
