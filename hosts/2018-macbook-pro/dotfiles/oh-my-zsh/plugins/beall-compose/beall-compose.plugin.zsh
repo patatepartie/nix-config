3c3d97a1681c715178400b3ae743420273793bd5
Cyril Ledru <cyril@lev-art.com>
2025-12-03 14:15:40 +0900

    MBP2018/2023 - Modernize BC plugin
    Use the newish --build argument of the docker compose run commands.
    Add new containers for Brag.
    Document new prefixes.

--- hosts/2018-macbook-pro/dotfiles/oh-my-zsh/plugins/beall-compose/beall-compose.plugin.zsh
+++ hosts/2018-macbook-pro/dotfiles/oh-my-zsh/plugins/beall-compose/beall-compose.plugin.zsh
@@ -4,6 +4,8 @@
 # Hachi: h
 # Beair: a
 # Believe: l
+# Betrained: t
+# Beproduced: p
 # Beship: s
 # Beaware: w
 # Db-tools: d
@@ -13,24 +15,29 @@
 
 alias bc='beall-compose'
 alias bcb='beall-compose build'
-alias bccu='beall-compose up mysql redis duckling'
+alias bccu='beall-compose up mysql redis'
 alias bcwu='beall-compose up --build webchat'
 alias bchu='beall-compose up --build hachi hachi_threaded hachi_async sidekiq'
 alias bchuh='beall-compose -a honeycomb up --build hachi hachi_threaded hachi_async sidekiq'
 alias bcau='beall-compose up --build beair beair_sidekiq'
 alias bcauh='beall-compose -a honeycomb up --build beair beair_sidekiq'
-alias bclu='beall-compose up --build believe believe_celery_matching'
-alias bcluh='beall-compose -a honeycomb up --build believe believe_celery_matching'
+alias bclu='beall-compose up --build believe believe_celery_matching believe_celery_brag believe_celery_brag_admin'
+alias bcluh='beall-compose -a honeycomb up --build believe believe_celery_matching believe_celery_brag believe_celery_brag_admin'
+alias bctu='beall-compose -s betrained up --build betrained betrained_sidekiq'
+alias bctuh='beall-compose -s betrained -a honeycomb up --build betrained betrained_sidekiq'
+alias bcpu='beall-compose -s beproduced up --build beproduced_web beproduced_celery'
+alias bcpuh='beall-compose -s beproduced -a honeycomb up --build beproduced_web beproduced_celery'
 alias bcsu='beall-compose -s beship up --build beship'
 alias bcs='beall-compose -s beship'
 alias bcsd='beall-compose -s beship.dev'
 alias bcst='beall-compose -s beship.test'
 alias bcss='beall-compose -s beship.dev up --build beship-sync'
-alias bcsr='beall-compose -s beship.dev run --rm beship bash'
-alias bcsts='beall-compose -s beship.test run --rm beship bash'
+alias bcsr='beall-compose -s beship.dev run --rm --build beship bash'
+alias bcsts='beall-compose -s beship.test run --rm --build beship bash'
 alias bcsto='beall-compose -s beship.test stop chrome'
 alias bcd='beall-compose -s db-tools'
-alias bcdr='beall-compose -s db-tools run --rm db-tools'
-alias bcdih='beall-compose -s db-tools run --rm db-tools import hachi'
-alias bcdia='beall-compose -s db-tools run --rm db-tools import beair'
-alias bcdis='beall-compose -s db-tools run --rm db-tools import beship'
+alias bcdr='beall-compose -s db-tools run --rm --build db-tools'
+alias bcdih='beall-compose -s db-tools run --rm --build db-tools import hachi'
+alias bcdia='beall-compose -s db-tools run --rm --build db-tools import beair'
+alias bcdis='beall-compose -s db-tools run --rm --build db-tools import beship'
+alias bcdit='beall-compose -s db-tools run --rm --build db-tools import betrained'
