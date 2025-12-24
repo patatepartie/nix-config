5955e3d0baafe9249256b76808861a051abd2de4
Cyril Ledru <cyril@lev-art.com>
2025-12-24 10:57:09 +0900

    Add extension to AZ cli, add terraform cli and remove obsidian
    Obsidian is already managed by homebrew so it shouldn't be also installed by nix.
    
    Also re-order the casks and home packages alphabetically.

--- hosts/2023-macbook-pro/modules/apps/casks.nix
+++ hosts/2023-macbook-pro/modules/apps/casks.nix
@@ -7,12 +7,12 @@
     { name = "ghostty"; greedy = true; }
     { name = "google-drive"; greedy = true; }
     { name = "karabiner-elements"; greedy = true; }
-    { name = "windows-app"; greedy = true; }
-    { name = "obsidian"; greedy = true; }
     { name = "nordvpn"; greedy = true; }
+    { name = "obsidian"; greedy = true; }
     { name = "p4v"; greedy = true; }
     { name = "spotify"; greedy = true; }
     { name = "vlc"; greedy = true; }
+    { name = "windows-app"; greedy = true; }
   ] ++ [
     "firefox"
     "google-chrome"
